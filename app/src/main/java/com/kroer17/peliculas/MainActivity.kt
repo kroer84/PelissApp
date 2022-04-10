5f6eae3f0ddd3145ba17a41c21831167134f3a3f
Miguel Jiménez Gonzélez <kroer17@hotmail.com>
2022-04-10 13:48:02 -0500

    first commit

---
+++ app/src/main/java/com/kroer17/peliculas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kroer17.peliculas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
